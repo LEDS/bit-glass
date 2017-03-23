d6d435d826afadfab155f773c7681eb7593f3af8
Sávio Santos <serra.santos.s@gmail.com>
2017-03-23 20:52:02 -0300

    Initial commit

---
+++ bitGlass.Bdd/Steps/SumValuesSteps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace bitGlass.Bdd.Steps
+{
+    [Binding]
+    public class SumValuesSteps
+    {
+        private double[] _inputs = new double[2];
+        private double _result;
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int first)
+        {
+            _inputs[0] = first;
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int second)
+        {
+            _inputs[1] = second;
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            _result = _inputs.Sum();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int result)
+        {
+            Assert.AreEqual(result, _result);
+        }
+    }
+}
---
+++ bitGlass/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="bitGlass.MvcApplication" Language="C#" %>
